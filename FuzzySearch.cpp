5c6d8a6c05de6af6f4a7d78e7caa09de0b3c8abe
unknown <krisprad@yahoo.co.uk>
2020-07-21 11:37:39 +0100

    Differentiating likes of 'dd' and 'd' in syl_string

--- FuzzySearch.cpp
+++ FuzzySearch.cpp
@@ -2,7 +2,7 @@
 
 
 template<typename T>
-bool compare_ind_str(const  T& s1, const T& s2) {
+bool compare_str(const  T& s1, const T& s2) {
 	if (s1 == s2) {
 		cout << "'" << s1.c_str() << "' and '" << s2.c_str() << "'\t\t are same\n";
 		return true;
@@ -19,17 +19,18 @@ void compare_strings() {
 	const T s2 = "Andhra Jyothi";
 	const T s3 = "aandhra Jyoti";
 	const T s4 = "andra jyoothi";
-	compare_ind_str(s1, s2); // match
-	compare_ind_str(s1, s3); // match
-	compare_ind_str(s1, s4); // match
-	compare_ind_str(s3, s4); // match
+	compare_str(s1, s2); // match
+	compare_str(s1, s3); // match
+	compare_str(s1, s4); // match
+	compare_str(s3, s4); // match
 
-	compare_ind_str(T("avatar "), T("Avathar"));  // match
-	compare_ind_str(T("avatar"), T("avadar"));  // mismatch
-	compare_ind_str(T("sampath"), T("sampat"));  // mismatch
-	compare_ind_str(T("sampath"), T("sampat "));  // match
-	compare_ind_str(T("Srinivas "), T("Sreenivas"));  // mismatch for ind_string but match for syl_string
-	compare_ind_str(T("Srinivaas "), T("Sreenivass"));  // mismatch for ind_string but match for syl_string
+	compare_str(T("avatar "), T("Avathar"));  // match
+	compare_str(T("avatar"), T("avadar"));  // mismatch
+	compare_str(T("sampath"), T("sampat"));  // mismatch
+	compare_str(T("sampath"), T("sampat "));  // match
+	compare_str(T("Srinivas "), T("Sreenivas"));  // mismatch for ind_string but match for syl_string
+	compare_str(T("Srinivaas "), T("Sreenivass"));  // mismatch for ind_string but match for syl_string because of 'ee' vs 'i'
+	compare_str(T("Udeep "), T("Uddeep"));  // match for ind_string but mismatch for syl_string. because of 'dd' vs 'd'
 }
 
 int main(int argc, char** argv) {
@@ -51,6 +52,7 @@ int main(int argc, char** argv) {
 'sampath' and 'sampat '          are same
 'Srinivas ' and 'Sreenivas'              are not same
 'Srinivaas ' and 'Sreenivass'            are not same
+'Udeep ' and 'Uddeep'            are same
 
 ----- syl_string comparision -------
 'Andhra Jyothi' and 'Andhra Jyothi'              are same
@@ -63,4 +65,5 @@ int main(int argc, char** argv) {
 'sampath' and 'sampat '          are same
 'Srinivas ' and 'Sreenivas'              are same
 'Srinivaas ' and 'Sreenivass'            are not same
+'Udeep ' and 'Uddeep'            are not same
 */
